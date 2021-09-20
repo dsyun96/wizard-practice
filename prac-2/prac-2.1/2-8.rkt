d6a612866cb46e75445bbd0d4024339c5fd3d33d
YunGoon <dsyun96@naver.com>
2021-09-20 13:52:57 +0900

    연습문제 2.8

---
+++ prac-2/prac-2.1/2-8.rkt
@@ -0,0 +1,14 @@
+;; Alyssa가 했던 것처럼, 구간 값의 뺄셈을 구하는 방법을 설명하고,
+;; 이를 sub-interval 프로시저로 정의하라.
+
+(define (make-inteval a b) (cons a b))
+
+(define (lower-bound x) (car x))
+
+(define (upper-bound x) (cdr x))
+
+(define (sub-interval a b)
+  (make-interval (- (lower-bound a)
+                    (lower-bound b))
+                 (- (upper-bound a)
+                    (upper-bound b))))
